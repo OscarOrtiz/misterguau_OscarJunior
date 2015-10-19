b27b05813be851e1c934dafe20b35a84f68c1d5c
unknown <93186.joan23@415-PORTC1195NC.joan23.local>
2015-10-19 13:07:05 +0200

    v1

---
+++ listaAnimales.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title></title>
+	</head>
+	<body>
+<?php
+			//realizamos la conexión con mysql
+			$con = mysqli_connect('172.24.49.56', 'root', '', 'bd_botiga_animals');
+
+			$sql = "SELECT * FROM tbl_anunci WHERE ";
+
+
+			$sql.= "anu_tipus = 'Perdut'";
+			$datos = mysqli_query($con, $sql);
+		
+
+			while($tbl_anunci = mysqli_fetch_array($datos)){
+
+
+				echo "<b>Id:</b>" .$tbl_anunci['anu_id']."<br/>";
+				echo "<b>Nombre:</b>" .$tbl_anunci['anu_nom']."<br/>";
+				echo "<b>Anuncio:</b>" .$tbl_anunci['anu_contingut']."<br/>";
+				echo "<b>Fecha:</b>" .$tbl_anunci['anu_data']."<br/>";
+
+			}	
+			
+			//cerramos la conexión con la base de datos
+			mysqli_close($con);
+		?>
+</body>
+</html>
